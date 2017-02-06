c2530d66048784dadd809589e14dddf47146713b
Dan-abid <abd5@umbc.edu>
2017-02-06 10:30:16 -0500

    changed loop counter

--- smaller_data.m
+++ smaller_data.m
@@ -1,3 +1,4 @@
+clear all;
 sig_name = 'mod_BPSK_snr_10';
 idx = 1:1000;
 %1M samples/second 128 micro seconds i.e 128 samples
@@ -13,11 +14,12 @@ for t=1:length(x)
     xdft(t) = x(t)*shift(t);
 end
 count=0;
-for i=1:10
-    sub(i,:)=xdft(count+1:count+10);
-    count=count+10;
+x=10;
+for i=1:x
+    sub(i,:)=xdft(count+1:count+x);
+    count=count+x;
 end
-for m=1:10
+for m=1:x
     other_dat(:,m)=abs(fft(sub(:,m)));
 end
 figure
